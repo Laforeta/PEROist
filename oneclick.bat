ac9cb567cee170e5cf007ce3051f03a8134d97f4
Laforeta <amadeus7806@gmail.com>
2015-07-09 16:18:17 +1200

    Self-test routines
    Adding checks for scripts and binary to main batches.

--- oneclick.bat
+++ oneclick.bat
@@ -3,12 +3,20 @@ SETLOCAL ENABLEDELAYEDEXPANSION ENABLEEXTENSIONS
 SET ME=%~n0
 SET PARENT=%~dp0
 
-ECHO Preparing...
+ECHO Loading scripts...
+FOR /F %%f in ('type data\modules') DO (
+	IF NOT EXIST %%f ECHO %%F is missing && GOTO GENERIC_FAIL
+)
+ECHO Loading modules...
+CALL SELFTEST > selftest.log 2>&1
+if %errorlevel% neq 0 GOTO GENERIC_FAIL
+
+ECHO Preparing folders...
 DEL /q *.hack.swf > nul 2>&1
 CALL CLEANUP > nul 2>&1
+MKDIR temp
 
 ECHO Importing files...
-MKDIR temp
 COPY *.swf temp
 
 CALL EXTRACT > extract.log 2>&1
@@ -20,13 +28,19 @@ COPY %parent%temp\*.hack.swf %parent%
 
 CD %PARENT%
 ECHO Saving log to log_lastrun.txt...
-COPY extract.log + scale.log + replace.log log_lastrun.txt > nul 2>&1
+COPY selftest.log + extract.log + scale.log + replace.log log_lastrun.txt > nul 2>&1
 ECHO Removing temporary files...
 CALL CLEANUP > nul 2>&1
 
 ENDLOCAL
-
 ECHO ----------------
 ECHO ~~Run Complete~~
 ECHO ----------------
 PAUSE
+
+:GENERIC_FAIL
+ENDLOCAL
+ECHO -------------
+ECHO Run Failed :(
+ECHO -------------
+EXIT /B 1
--- selftest.cmd
+++ selftest.cmd
@@ -3,48 +3,50 @@ SETLOCAL ENABLEDELAYEDEXPANSION ENABLEEXTENSIONS
 SET ME=%~n0
 SET PARENT=%~dp0
 
-ECHO Performing self-tests, please wait...
+ECHO Performing self-tests, please wait...>con
 MKDIR temp
 CD bin
 
-ECHO Testing ffdec..
+ECHO Testing ffdec..>con
 java -jar %PARENT%bin/ffdec/ffdec.jar --help
 if %errorlevel% neq 0 GOTO FFDEC_FAIL
 
-ECHO Testing ImageMagick...
+ECHO Testing ImageMagick...>con
 convert %PARENT%\data\selftest.gif %PARENT%temp\selftest.png
 if %errorlevel% neq 0 GOTO IM_FAIL
 
-ECHO Testing Waifu2x...
+ECHO Testing Waifu2x...>con
 waifu2x-converter.exe -i %PARENT%temp\selftest.png -o %PARENT%temp\selftest_2x.png -m noise_scale --noise_level 1
 if %errorlevel% neq 0 GOTO Waifu2x_FAIL
 
 GOTO PASS
 
 :IM_FAIL
-ECHO An error has occured while testing ImageMagick
-ECHO Please view log_selftest.txt for detailed error message
+ECHO An error has occured while testing ImageMagick>con
+ECHO Please view log_selftest.txt for detailed error message>con
 GOTO ABORT
 
 :FFDEC_FAIL
-ECHO An error has occured while testing ffdec
-ECHO Please view log_selftest.txt for detailed error message
+ECHO An error has occured while testing ffdec>con
+ECHO Please view log_selftest.txt for detailed error message>con
 GOTO ABORT
 
 :WAIFU2X_FAIL
-ECHO An error has occured while testing Waifu2x
-ECHO Please view log_selftest.txt for detailed error message
+ECHO An error has occured while testing Waifu2x>con
+ECHO Please view log_selftest.txt for detailed error message>con
 GOTO ABORT
 
 :PASS
-ECHO ----------------
-ECHO Self-test passed
-ECHO ----------------
+ENDLOCAL
+ECHO ---------------->con
+ECHO Self-test passed>con
+ECHO ---------------->con
 EXIT /B 0
 
 :ABORT
-ECHO ----------------
-ECHO Self-test failed
-ECHO ----------------
+ENDLOCAL
+ECHO ---------------->con
+ECHO Self-test failed>con
+ECHO ---------------->con
 PAUSE
 EXIT /B 1
