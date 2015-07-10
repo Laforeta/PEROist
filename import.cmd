9defe424b325b15ad6a4c5a0e777aa6e214495d0
Laforet <amadeus7806@gmail.com>
2015-07-10 21:59:47 +1200

    Improved file sorting
    IMPORT will now skip files if it encounters a read error and move the
    offending file to \exception\

--- import.cmd
+++ import.cmd
@@ -1,17 +1,23 @@
-@ECHO OFF
-SETLOCAL ENABLEDELAYEDEXPANSION ENABLEEXTENSIONS
-SET ME=%~n0
-SET PARENT=%~dp0
-
-REM Identify sprite class by the existence of Character 25. 
-
+REM Identify sprite class by the existence of CharacterId 17
+MKDIR error
 MKDIR temp\abyssal
 For %%f in (*.swf) DO (
-	java -jar %PARENT%bin\ffdec\ffdec.jar -format image:png -selectid 25 -export image "%PARENT%temp" %%f
-	IF NOT EXIST %PARENT%temp\25.png COPY %%f %PARENT%temp\abyssal
-	IF EXIST %PARENT%temp\25.png COPY %%f %PARENT%temp
-	DEL /q %PARENT%temp\25.png
+	java -jar %PARENT%bin\ffdec\ffdec.jar -onerror ignore -format image:png -selectid 1 -export image "%PARENT%temp" %%f
+	java -jar %PARENT%bin\ffdec\ffdec.jar -onerror ignore -format image:png -selectid 17 -export image "%PARENT%temp" %%f
+	IF NOT EXIST %PARENT%temp\1.png (
+		ECHO Failed to import %%f, skipping this file...>con
+		ECHO Failed to import %%f, skipping this file...
+		COPY %%f %PARENT%error 
+	) ELSE IF NOT EXIST %PARENT%temp\17.png (
+		ECHO Importing %%f...>con
+		ECHO Importing %%f...
+		COPY %%f %PARENT%temp\abyssal
+	) ELSE (
+		ECHO Importing %%f...>con
+		ECHO Importing %%f
+		COPY %%f %PARENT%temp
+	)
+	DEL /q %PARENT%temp\*.png
 )
 
-ENDLOCAL
 EXIT /B 0
