ef9ebad516523cb46e5c293422f8d260c550d851
Laforeta <amadeus7806@gmail.com>
2015-07-12 15:37:47 +1200

    New subroutine based structure for SCALE

--- scale.cmd
+++ scale.cmd
@@ -12,148 +12,38 @@ ECHO ------------->CON
 ECHO Scaling Start>CON
 ECHO ------------->CON
 
-REM Establish working path - waifu2x-cpp have trouble reading the models otherwise
-CD bin
+REM Establish working path - waifu2x-cpp seems to have trouble reading the models otherwise
+CD "%PARENT%\bin"
 
-REM Scale kanmusu sprite #1
+REM Merged scale routines for KANMUSU
 FOR %%f IN (%PARENT%temp\*) DO (
 	SETLOCAL ENABLEDELAYEDEXPANSION
 	SET /a TARGET=1
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!.png -o %%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-REM Scale kanmusu sprite #3
-FOR %%f IN (%PARENT%temp\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL SCALE
 	SET /a TARGET=3
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!.png -o %%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-
-REM Scale kanmusu sprite #5
-FOR %%f IN (%PARENT%temp\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL SCALE
 	SET /a TARGET=5
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!.png -o %%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-
-REM Scale kanmusu sprite #7
-FOR %%f IN (%PARENT%temp\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL SCALE
 	SET /a TARGET=7
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!.png -o %%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-
-REM Scale kanmusu sprite #17 (Main Sprite w/alpha)
-FOR %%f IN (%PARENT%temp\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL SCALE
 	SET /a TARGET=17
-	ECHO Processing Alpha of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -separate %%f_images\!TARGET!_alpha.png
-	ECHO Processing RGB of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -threshold 100%% +channel %%f_images\!TARGET!_rgb.png
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled RGB for Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!_rgb.png -o %%f_images\2x!TARGET!_rgb.png
-	ECHO Generating Scaled alpha for Image !TARGET! in %%f
-	waifu2x-converter -m scale -i %%f_images\!TARGET!_alpha.png -o %%f_images\2x!TARGET!_alpha.png
-	ECHO Combine RGB with Alpha
-	convert %%f_images\2x!TARGET!_rgb.png %%f_images\2x!TARGET!_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-REM Scale kanmusu sprite #19 (Main Sprite - Damaged w/alpha)
-FOR %%f IN (%PARENT%temp\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL SCALE_ALPHA
 	SET /a TARGET=19
-	ECHO Processing Alpha of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -separate %%f_images\!TARGET!_alpha.png
-	ECHO Processing RGB of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -threshold 100%% +channel %%f_images\!TARGET!_rgb.png
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled RGB for Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!_rgb.png -o %%f_images\2x!TARGET!_rgb.png
-	ECHO Generating Scaled alpha for Image !TARGET! in %%f
-	waifu2x-converter -m scale -i %%f_images\!TARGET!_alpha.png -o %%f_images\2x!TARGET!_alpha.png
-	ECHO Combine RGB with Alpha
-	convert %%f_images\2x!TARGET!_rgb.png %%f_images\2x!TARGET!_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-REM Scale kanmusu sprite #27 (Supply Banner w/alpha)
-FOR %%f IN (%PARENT%temp\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL SCALE_ALPHA
 	SET /a TARGET=27
-	ECHO Processing Alpha of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -separate %%f_images\!TARGET!_alpha.png
-	ECHO Processing RGB of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -threshold 100%% +channel %%f_images\!TARGET!_rgb.png
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled RGB for Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!_rgb.png -o %%f_images\2x!TARGET!_rgb.png
-	ECHO Generating Scaled alpha for Image !TARGET! in %%f
-	waifu2x-converter -m scale -i %%f_images\!TARGET!_alpha.png -o %%f_images\2x!TARGET!_alpha.png
-	ECHO Combine RGB with Alpha
-	convert %%f_images\2x!TARGET!_rgb.png %%f_images\2x!TARGET!_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-REM Scale kanmusu sprite #29 (Supply Banner - Damaged w/alpha)
-FOR %%f IN (%PARENT%temp\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL SCALE_ALPHA
 	SET /a TARGET=29
-	ECHO Processing Alpha of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -separate %%f_images\!TARGET!_alpha.png
-	ECHO Processing RGB of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -threshold 100%% +channel %%f_images\!TARGET!_rgb.png
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled RGB for Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!_rgb.png -o %%f_images\2x!TARGET!_rgb.png
-	ECHO Generating Scaled alpha for Image !TARGET! in %%f
-	waifu2x-converter -m scale -i %%f_images\!TARGET!_alpha.png -o %%f_images\2x!TARGET!_alpha.png
-	ECHO Combine RGB with Alpha
-	convert %%f_images\2x!TARGET!_rgb.png %%f_images\2x!TARGET!_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%%f_images\2x!TARGET!.png
+	CALL SCALE_ALPHA
 	ENDLOCAL
 )
 
-REM Scale Abyssal sprite #1 (Banner)
+REM Merged scale routines for ABYSSAL
 FOR %%f IN (%PARENT%temp\abyssal\*) DO (
 	SETLOCAL ENABLEDELAYEDEXPANSION
 	SET /a TARGET=1
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!.png -o %%f_images\2x!TARGET!.png
-	ENDLOCAL
-)
-
-REM Scale Abyssal Sprite 3 (Main sprite w/ alpha)
-FOR %%f IN (%PARENT%temp\abyssal\*) DO (
-	SETLOCAL ENABLEDELAYEDEXPANSION
+	CALL :SCALE
 	SET /a TARGET=3
-	ECHO Processing Alpha of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -separate %%f_images\!TARGET!_alpha.png
-	ECHO Processing RGB of Image !TARGET! in %%f...
-	convert %%f_images\!TARGET!.png -channel alpha -threshold 100%% +channel %%f_images\!TARGET!_rgb.png
-	ECHO Generating Scaled Image !TARGET! in %%f>CON
-	ECHO Generating Scaled RGB for Image !TARGET! in %%f
-	waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!_rgb.png -o %%f_images\2x!TARGET!_rgb.png
-	ECHO Generating Scaled alpha for Image !TARGET! in %%f
-	waifu2x-converter -m scale -i %%f_images\!TARGET!_alpha.png -o %%f_images\2x!TARGET!_alpha.png
-	ECHO Combine RGB with Alpha
-	convert %%f_images\2x!TARGET!_rgb.png %%f_images\2x!TARGET!_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%%f_images\2x!TARGET!.png
+	CALL :SCALE_ALPHA
 	ENDLOCAL
 )
 
@@ -161,3 +51,32 @@ ENDLOCAL
 ECHO ------------>CON
 ECHO Scaling Done>CON
 ECHO ------------>CON
+EXIT /B 0
+
+ECHO -------------->CON
+ECHO Scaling FAILED>CON
+ECHO -------------->CON
+EXIT /B 1
+
+
+:SCALE
+ECHO Generating Scaled Image !TARGET! in %%f>CON
+ECHO Generating Scaled Image !TARGET! in %%f
+waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!.png -o %%f_images\2x!TARGET!.png
+if %errorlevel% neq 0 ECHO Scaling failed for %%f>2
+GOTO:EOF
+
+:SCALE_ALPHA
+ECHO 
+ECHO Processing Alpha of Image !TARGET! in %%f...
+convert %%f_images\!TARGET!.png -channel alpha -separate %%f_images\!TARGET!_alpha.png
+ECHO Processing RGB of Image !TARGET! in %%f...
+convert %%f_images\!TARGET!.png -channel alpha -threshold 100%% +channel %%f_images\!TARGET!_rgb.png
+ECHO Generating Scaled Image !TARGET! in %%f>CON
+ECHO Generating Scaled RGB for Image !TARGET! in %%f
+waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!_rgb.png -o %%f_images\2x!TARGET!_rgb.png
+ECHO Generating Scaled alpha for Image !TARGET! in %%f
+waifu2x-converter -m scale -i %%f_images\!TARGET!_alpha.png -o %%f_images\2x!TARGET!_alpha.png
+ECHO Combine RGB with Alpha
+convert %%f_images\2x!TARGET!_rgb.png %%f_images\2x!TARGET!_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%%f_images\2x!TARGET!.png
+GOTO:EOF
