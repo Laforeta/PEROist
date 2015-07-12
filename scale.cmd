14760cf034ee9e3af79b6e31cdf0b8782ccbe757
Laforeta <amadeus7806@gmail.com>
2015-07-12 16:05:05 +1200

    Fixed subroutine syntax
    Fixed syntax so it actually runs okay now

--- scale.cmd
+++ scale.cmd
@@ -12,12 +12,13 @@ ECHO ------------->CON
 ECHO Scaling Start>CON
 ECHO ------------->CON
 
-REM Establish working path - waifu2x-cpp seems to have trouble reading the models otherwise
+REM Establish working path - waifu2x-cpp seems to have trouble finding the models without doing this
 CD "%PARENT%\bin"
 
 REM Merged scale routines for KANMUSU
 FOR %%f IN (%PARENT%temp\*) DO (
 	SETLOCAL ENABLEDELAYEDEXPANSION
+	SET FILENAME=%%f
 	SET /a TARGET=1
 	CALL SCALE
 	SET /a TARGET=3
@@ -37,9 +38,12 @@ FOR %%f IN (%PARENT%temp\*) DO (
 	ENDLOCAL
 )
 
+REM Add a loop for EXCEPTION when it's done
+
 REM Merged scale routines for ABYSSAL
 FOR %%f IN (%PARENT%temp\abyssal\*) DO (
 	SETLOCAL ENABLEDELAYEDEXPANSION
+	SET FILENAME=%%f
 	SET /a TARGET=1
 	CALL :SCALE
 	SET /a TARGET=3
@@ -58,25 +62,28 @@ ECHO Scaling FAILED>CON
 ECHO -------------->CON
 EXIT /B 1
 
+REM Check the subroutines below, they appear to breakdown when there is a space in "%%f_images"
 
 :SCALE
-ECHO Generating Scaled Image !TARGET! in %%f>CON
-ECHO Generating Scaled Image !TARGET! in %%f
-waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!.png -o %%f_images\2x!TARGET!.png
-if %errorlevel% neq 0 ECHO Scaling failed for %%f>2
+ECHO Generating Scaled Image %TARGET% in %FILENAME%>CON
+ECHO Generating Scaled Image %TARGET% in %FILENAME%
+waifu2x-converter -m noise_scale --noise_level 1 -i %FILENAME%_images\%TARGET%.png -o %FILENAME%_images\2x%TARGET%.png
 GOTO:EOF
 
 :SCALE_ALPHA
-ECHO 
-ECHO Processing Alpha of Image !TARGET! in %%f...
-convert %%f_images\!TARGET!.png -channel alpha -separate %%f_images\!TARGET!_alpha.png
-ECHO Processing RGB of Image !TARGET! in %%f...
-convert %%f_images\!TARGET!.png -channel alpha -threshold 100%% +channel %%f_images\!TARGET!_rgb.png
-ECHO Generating Scaled Image !TARGET! in %%f>CON
-ECHO Generating Scaled RGB for Image !TARGET! in %%f
-waifu2x-converter -m noise_scale --noise_level 1 -i %%f_images\!TARGET!_rgb.png -o %%f_images\2x!TARGET!_rgb.png
-ECHO Generating Scaled alpha for Image !TARGET! in %%f
-waifu2x-converter -m scale -i %%f_images\!TARGET!_alpha.png -o %%f_images\2x!TARGET!_alpha.png
+ECHO Generating Scaled Image %TARGET% in %FILENAME%>CON
+ECHO Processing Alpha of Image %TARGET% in %FILENAME%...
+convert %FILENAME%_images\%TARGET%.png -channel alpha -separate %FILENAME%_images\%TARGET%_alpha.png
+ECHO Processing RGB of Image %TARGET% in %FILENAME%...
+convert %FILENAME%_images\%TARGET%.png -channel alpha -threshold 100%% +channel %FILENAME%_images\%TARGET%_rgb.png
+ECHO Generating Scaled RGB for Image %TARGET% in %FILENAME%
+waifu2x-converter -m noise_scale --noise_level 1 -i %FILENAME%_images\%TARGET%_rgb.png -o %FILENAME%_images\2x%TARGET%_rgb.png
+ECHO Generating Scaled alpha for Image %TARGET% in %FILENAME%
+waifu2x-converter -m scale -i %FILENAME%_images\%TARGET%_alpha.png -o %FILENAME%_images\2x%TARGET%_alpha.png
 ECHO Combine RGB with Alpha
-convert %%f_images\2x!TARGET!_rgb.png %%f_images\2x!TARGET!_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%%f_images\2x!TARGET!.png
+convert %FILENAME%_images\2x%TARGET%_rgb.png %FILENAME%_images\2x%TARGET%_alpha.png -alpha off -compose CopyOpacity -composite PNG32:%FILENAME%_images\2x%TARGET%.png
+GOTO:EOF
+
+:SHARPEN
+ECHO Adding some finishing touches to %TARGET% in %FILENAME%>CON
 GOTO:EOF
