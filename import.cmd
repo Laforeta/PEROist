bc0e90ac44bf2682bb205dab536e204246c1834f
Laforeta <amadeus7806@gmail.com>
2015-07-13 18:16:15 +1200

    Block process of files
    - Added a counter so IMPORT will stop after importing the 5th file
    
    - IMPORT will now MOVE files to /temp/ instead of COPY

--- import.cmd
+++ import.cmd
@@ -7,23 +7,33 @@ SETLOCAL
 SET ME=%~n0
 SET PARENT=%~dp0
 
-MKDIR "%PARENT%error"
+REM Why does this work? what is %h defined as here? 
+MKDIR "%PARENT%temp\error"
 MKDIR "%PARENT%temp\abyssal"
 MKDIR "%PARENT%temp\kanmusu"
+SET BLOCKSIZE=5
+FOR /F "tokens=1* delims=[]" %%g in ('DIR /A-D /B *.swf ^|find /v /n ""') DO (
+	MOVE "%%~nxh" "%PARENT%temp"
+	ECHO Importing file %%g of %BLOCKSIZE%...
+	if %%g==%BLOCKSIZE% GOTO SORTING
+)
+
+:SORTING
+CD temp
 For %%f in (*.swf) DO (
 	java -jar "%PARENT%bin\ffdec\ffdec.jar" -onerror ignore -format image:png -selectid 1 -export image "%PARENT%temp" "%%f"
 	java -jar "%PARENT%bin\ffdec\ffdec.jar" -onerror ignore -format image:png -selectid 17 -export image "%PARENT%temp" "%%f"
 	IF NOT EXIST "%PARENT%temp\1.png" (
-		ECHO Failed to import %%f, skipping this file...>con
-		ECHO Failed to import %%f, skipping this file...
+		ECHO Failed to detect file type of %%f, skipping this file...>con
+		ECHO Failed to detect file type of %%f, skipping this file...
 		COPY "%%f" "%PARENT%error\"
 	) ELSE IF NOT EXIST "%PARENT%temp\17.png" (
-		ECHO Importing %%f...>con
-		ECHO Importing %%f...
+		ECHO Detecting file type of %%f...>con
+		ECHO Detecting file type of %%f...
 		COPY "%%f" "%PARENT%temp\abyssal\"
 	) ELSE (
-		ECHO Importing %%f...>con
-		ECHO Importing %%f
+		ECHO Detecting file type of %%f...>con
+		ECHO Detecting file type of %%f
 		COPY "%%f" "%PARENT%temp\kanmusu\"
 	)
 	DEL /q "%PARENT%temp\*.png"
