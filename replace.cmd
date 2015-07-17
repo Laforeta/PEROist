2766f83d5785580752bea06bf249c46212211d31
Laforeta <amadeus7806@gmail.com>
2015-07-17 12:37:30 +1200

    Removed a redundant GOTO in REPLACE

--- replace.cmd
+++ replace.cmd
@@ -11,14 +11,12 @@ ECHO ----------------->CON
 REM Replace images in kanmusu sprites
 CD "%PARENT%temp\kanmusu"
 FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\kanmusu\*.swf"') DO (
-	IF NOT EXIST *.swf GOTO ABYSSAL
 	ECHO Replacing images in %%f...>CON
 	ECHO Replacing images in %%f...
 	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "%%fh" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png" 6 "%%f_images\2x5.png" 8 "%%f_images\2x7.png" 18 "%%f_images\2x17.png" 20 "%%f_images\2x19.png" 28 "%%f_images\2x27.png" 30 "%%f_images\2x29.png"
 )
 REN *.swfh *.hack.swf
 
-:abyssal
 REM Replace images in abyssal sprites
 CD "%PARENT%temp\abyssal"
 FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\abyssal\*.swf"') DO (
