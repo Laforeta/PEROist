6b71226dd9972ee9d235ffdd08ddad702266f6fd
Laforet <amadeus7806@gmail.com>
2015-07-22 03:09:38 +1200

    Apply binary alpha mask

--- scale.cmd
+++ scale.cmd
@@ -18,6 +18,7 @@ ECHO ------------->CON
 REM These processes depend on being inside the right working directory to run
 REM Binaries must be called as variables using their full path
 SET IM="%PARENT%bin\convert.exe"
+SET STREAM="%PARENT%bin\stream.exe"
 IF NOT DEFINED JPEG_QUALITY SET /a JPEG_QUALITY=92
 
 IF NOT DEFINED AMD64 SET /a AMD64=1
@@ -164,6 +165,10 @@ ECHO Converting Scaled RGB for Image %TARGET% in %FILENAME% to JPEG bitmap...
 %IM% -quality %JPEG_QUALITY% 2x%TARGET%_rgb.png 2x%TARGET%.jpg
 ECHO Generating Scaled alpha for Image %TARGET% in %FILENAME%...
 %WAIFU2X% %MODEL% -m scale -i %TARGET%_alpha.png -o 2x%TARGET%_alpha.png
+ECHO Combining RGB with Alpha for Image %TARGET% in %FILENAME%...
+%IM% 2x%TARGET%_rgb.png 2x%TARGET%_alpha.png -alpha off -compose CopyOpacity -composite PNG32:2x%TARGET%.png
+ECHO Generate binary mask...
+%STREAM% -map a -storage-type char 2x%TARGET%.png 2x%TARGET%.bin
 GOTO:EOF
 
 :SHARPEN
