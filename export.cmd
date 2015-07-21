7413bb0b6604a2fdf3975ada41af8d0d25b9c751
Laforeta <amadeus7806@gmail.com>
2015-07-21 13:51:23 +1200

    Updated REPLACE and EXPORT
    - Pre-moddded files will now have their own subroutines in all modules
    
    - For the _mod subroutines, REPLACE will replace all 15 images. This is
    NOT ready for production.

--- export.cmd
+++ export.cmd
@@ -4,6 +4,7 @@ SET ME=%~n0
 SET PARENT=%~dp0
 
 ECHO Exporting files...>con
+
 FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\kanmusu\*.hack.swf"') DO (
 	ECHO Exporting %%g.hack.swf...
 	MOVE /y "%PARENT%temp\kanmusu\%%g.hack.swf" "%PARENT%output"
@@ -19,5 +20,21 @@ FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\abyssal\*.hack.sw
 	DEL /q "%PARENT%\%%g.swf"
 )
 MOVE /y "%PARENT%temp\abyssal\*.swf" "%PARENT%error"
+
+FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\kanmusu_mod\*.hack.swf"') DO (
+	ECHO Exporting %%g.hack.swf...
+	MOVE /y "%PARENT%temp\kanmusu\%%g.hack.swf" "%PARENT%output"
+	DEL /q "%PARENT%temp\kanmusu\%%g.hack.swf"
+	DEL /q "%PARENT%\%%g.hack.swf"
+)
+MOVE /y "%PARENT%temp\kanmusu\*.swf" "%PARENT%error"
+
+FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\abyssal_mod\*.hack.swf"') DO (
+	ECHO Exporting %%g.hack.swf...
+	MOVE /y "%PARENT%temp\kanmusu\%%g.hack.swf" "%PARENT%output"
+	DEL /q "%PARENT%temp\kanmusu\%%g.hack.swf"
+	DEL /q "%PARENT%\%%g.hack.swf"
+)
+MOVE /y "%PARENT%temp\kanmusu\*.swf" "%PARENT%error"
 
 ENDLOCAL
--- replace.cmd
+++ replace.cmd
@@ -8,7 +8,7 @@ ECHO ----------------->CON
 ECHO Replacement Start>CON
 ECHO ----------------->CON
 
-REM Replace images in kanmusu sprites
+:REPLACE_KANMUSU
 CD "%PARENT%temp\kanmusu"
 FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\kanmusu\*.swf"') DO (
 	ECHO Replacing images in %%f...>CON
@@ -17,7 +17,7 @@ FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\kanmusu\*.swf"') DO (
 )
 REN *.swfh *.hack.swf
 
-REM Replace images in abyssal sprites
+:REPLACE_ABYSSAL
 CD "%PARENT%temp\abyssal"
 FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\abyssal\*.swf"') DO (
 	ECHO Replacing images in %%f...>CON
@@ -25,6 +25,26 @@ FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\abyssal\*.swf"') DO (
 	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "%%fh" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png"
 )
 REN *.swfh *.hack.swf
+
+:REPLACE_KANMUSU_MOD
+CD "%PARENT%temp\kanmusu_mod"
+FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\kanmusu_mod\*.swf"') DO (
+	ECHO Replacing images in %%f...>CON
+	ECHO Replacing images in %%f...
+	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "2xlite_%%f" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png" 6 "%%f_images\2x5.png" 8 "%%f_images\2x7.png" 18 "%%f_images\2x17.png" 20 "%%f_images\2x19.png" 28 "%%f_images\2x27.png" 30 "%%f_images\2x29.png"
+	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "2xlite_%%f" "%%fh" 10 "%%f_images\2x9.png" 12 "%%f_images\2x11.png" 14 "%%f_images\2x13.png" 16 "%%f_images\2x15.png" 22 "%%f_images\2x21.png" 24 "%%f_images\2x23.png" 26 "%%f_images\2x25.png" 
+)
+REN *.swfh *.swf
+
+:REPLACE_ABYSSAL_MOD
+CD "%PARENT%temp\abyssal_mod"
+FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\abyssal\*.swf"') DO (
+	ECHO Replacing images in %%f...>CON
+	ECHO Replacing images in %%f...
+	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "%%fh" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png"
+)
+REN *.swfh *.swf
+
 
 ENDLOCAL
 ECHO ---------------->CON
