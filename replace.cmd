194a699bbeb1765a5b12d484fc67d24c2645d90b
Laforeta <amadeus7806@gmail.com>
2015-07-21 15:03:52 +1200

    Fixed a wrong CLI argument
    removeWithdependencies should be removeCharacterwithdependencies

--- replace.cmd
+++ replace.cmd
@@ -13,7 +13,8 @@ CD "%PARENT%temp\kanmusu"
 FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\kanmusu\*.swf"') DO (
 	ECHO Replacing images in %%f...>CON
 	ECHO Replacing images in %%f...
-	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "%%fh" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png" 6 "%%f_images\2x5.png" 8 "%%f_images\2x7.png" 18 "%%f_images\2x17.png" 20 "%%f_images\2x19.png" 28 "%%f_images\2x27.png" 30 "%%f_images\2x29.png"
+	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "2x%%f" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png" 6 "%%f_images\2x5.png" 8 "%%f_images\2x7.png" 18 "%%f_images\2x17.png" 20 "%%f_images\2x19.png" 28 "%%f_images\2x27.png" 30 "%%f_images\2x29.png"
+	java -jar "%PARENT%bin\ffdec\ffdec.jar" -removeCharacterwithdependencies "2x%%f" "%%fh" 1 3 5 7 17 19 27 29
 )
 REN *.swfh *.hack.swf
 
@@ -22,7 +23,8 @@ CD "%PARENT%temp\abyssal"
 FOR /f "delims=" %%f IN ('DIR /b /s /a:-d "%PARENT%temp\abyssal\*.swf"') DO (
 	ECHO Replacing images in %%f...>CON
 	ECHO Replacing images in %%f...
-	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "%%fh" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png"
+	java -jar "%PARENT%bin\ffdec\ffdec.jar" -replace "%%f" "2x%%f" 2 "%%f_images\2x1.png" 4 "%%f_images\2x3.png"
+	java -jar "%PARENT%bin\ffdec\ffdec.jar" -removeCharacterwithdependencies "2x%%f" "%%fh" 1 3
 )
 REN *.swfh *.hack.swf
 
