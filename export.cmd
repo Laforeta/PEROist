b0e4d12964f48f9428c48d92f3a3a67bbd23f5e1
Laforet <amadeus7806@gmail.com>
2015-07-22 21:10:32 +1200

    Final fix to EXPORT, resolves #21

--- export.cmd
+++ export.cmd
@@ -7,7 +7,7 @@ ECHO Exporting files...>con
 
 FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\kanmusu\*.hack.swf"') DO (
 	ECHO Exporting %%g.hack.swf...
-	MOVE /y "%PARENT%temp\kanmusu\%%g.hack.swf" "%PARENT%output"
+	COPY /y "%PARENT%temp\kanmusu\%%g.hack.swf" "%PARENT%output" && DEL /q "%PARENT%temp\kanmusu\%%g.hack.swf"
 	DEL /q "%PARENT%temp\kanmusu\%%g.swf"
 	DEL /q "%PARENT%\%%g.swf"
 )
@@ -15,29 +15,30 @@ MOVE /y "%PARENT%temp\kanmusu\*.swf" "%PARENT%error"
 
 FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\abyssal\*.hack.swf"') DO (
 	ECHO Exporting %%g.hack.swf...
-	MOVE /y "%PARENT%temp\abyssal\%%g.hack.swf" "%PARENT%output"
+	COPY /y "%PARENT%temp\abyssal\%%g.hack.swf" "%PARENT%output" &&	DEL /q "%PARENT%temp\abyssal\%%g.hack.swf"
 	DEL /q "%PARENT%temp\abyssal\%%g.swf"
 	DEL /q "%PARENT%\%%g.swf"
 )
 MOVE /y "%PARENT%temp\abyssal\*.swf" "%PARENT%error"
 
-FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\abyssal_mod\*.hack.swf"') DO (
+FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\kanmusu_mod\*.hack.out"') DO (
 	ECHO Exporting %%g.hack.swf...
-	MOVE /y "%PARENT%temp\abyssal_mod\%%g.hack.out" "%PARENT%output"
-	DEL /q "%PARENT%temp\abyssal_mod\%%g.hack.out"
+	COPY /y "%PARENT%temp\kanmusu_mod\%%g.hack.out" "%PARENT%output\%%g.hack.swf" && DEL /q "%PARENT%temp\kanmusu_mod\%%g.hack.out"
+	DEL /q "%PARENT%temp\kanmusu_mod\%%g.hack.swf"
 	DEL /q "%PARENT%\%%g.hack.swf"
 )
 MOVE /y "%PARENT%temp\kanmusu_mod\*.swf" "%PARENT%error"
 
-FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\abyssal_mod\*.hack.swf"') DO (
+FOR /f "tokens=1 delims=." %%g in ('DIR /A:-D /B "%PARENT%temp\abyssal_mod\*.hack.out"') DO (
 	ECHO Exporting %%g.hack.swf...
-	MOVE /y "%PARENT%temp\abyssal_mod\%%g.hack.out" "%PARENT%output"
-	DEL /q "%PARENT%temp\abyssal_mod\%%g.hack.out"
+	COPY /y "%PARENT%temp\abyssal_mod\%%g.hack.out" "%PARENT%output\%%g.hack.swf" && DEL /q "%PARENT%temp\abyssal_mod\%%g.hack.out"
+	DEL /q "%PARENT%temp\abyssal_mod\%%g.hack.swf"
 	DEL /q "%PARENT%\%%g.hack.swf"
 )
 MOVE /y "%PARENT%temp\abyssal_mod\*.swf" "%PARENT%error"
 
-CD %PARENT%output
-REN *.out *.swf
+REM Remove temp folder to prevent issues in repeat loops
+CD %PARENT%
+RD /s /q temp
 
 ENDLOCAL
