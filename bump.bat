93e89dec33c4571a7d8ee96a09a556aa65e12f55
Laforet <amadeus7806@gmail.com>
2015-07-28 00:51:00 +1200

    Fixed path reference in BUMP
    Extend the fixes in #3 to bump.bat. Previously it was not done for some
    reason.

--- bump.bat
+++ bump.bat
@@ -4,8 +4,8 @@ SET ME=%~n0
 SET PARENT=%~dp0
 ECHO This script will bump the "last-modified" timestamp of .hack.swf files to the current computer time. 
 ECHO OFF
-FOR /R %parent%kcs\resources\ %%f IN (*.hack.swf) DO COPY "%%f" + null "%%f"
-FOR /R %parent%kcs\sound\ %%f IN (*.hack.mp3) DO COPY "%%f" + null "%%f"
+FOR /R "%parent%kcs\resources\" %%f IN (*.hack.swf) DO COPY "%%f" + null "%%f"
+FOR /R "%parent%kcs\sound\" %%f IN (*.hack.mp3) DO COPY "%%f" + null "%%f"
 
 ENDLOCAL 
 ECHO Bump Completed
