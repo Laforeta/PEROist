a4b1678a3ecce0268e999c02bb76de24384caa1f
Laforeta <amadeus7806@gmail.com>
2015-07-29 17:49:12 +1200

    Added functionalities to INIGEN
    - Reorganised menu and interface
    
    - All functions are working except those to do with JSON (parse
    api_start2 and write results to apimodifier.json).
    
    - Routines for kaisyu and wedding need xy-origin data.
    
    - The method of deriving delta values from boko offsets needs to be
    tested globally and see whether they produce the intended results.

--- inigen.bat
+++ inigen.bat
@@ -4,17 +4,7 @@ SET ME=%~n0
 SET PARENT=%~dp0
 @ECHO Running %ME%
 
-REM Progress:
-REM Single file loader: Working
-REM Ini config loader: Working
-REM Multi file loader: Will load and expand values, but will not handle multiple values correctly. 
-REM Interactive Menu: Working
-REM Preview: Working (check on other OS)
-REM redefine offset: WORKING
-REM Write config to file: NOT WORKING
-REM Logging: NOT Working
-
-REM Set up the environment
+REM Declare binary paths
 SET IM="%PARENT%bin\convert.exe"
 SET STREAM="%PARENT%bin\stream.exe"
 SET VIEWER="%PARENT%bin\JPEGView\JPEGview.exe"
@@ -34,7 +24,7 @@ IF NOT EXIST *.hack.swf (
 	GOTO EXIT
 ) ELSE ( 
 	FOR /f "tokens=1 delims=." %%g IN ('DIR /b *.hack.swf') DO (
-		ECHO Found mod file %%g, analysing file....
+		ECHO Found file %%g.hack.swf found, analysing...
 		COPY "%%g.hack.swf" "%%PARENT%%temp"
 		java -jar "%PARENT%bin\ffdec\ffdec.jar" -format image:png -export image "%PARENT%temp\%%g.hack.swf_images" "%%g.hack.swf"
 		SET FILENAME=%%g
@@ -53,7 +43,8 @@ IF EXIST "%PARENT%input\%FILENAME%.config.ini" (
 ) ELSE IF EXIST "%PARENT%data\api_start2.json" (
 	ECHO Parse api_start2...
 ) ELSE (
-	ECHO No other information found, loading default values
+	ECHO No existing data found, loading default failsafe values.
+	PAUSE
 	FOR /f "tokens=1* delims=^=" %%f IN ('FIND "=" "%PARENT%data\default.config.ini"') DO (
 		SET %%f=%%g
 	)
@@ -68,71 +59,143 @@ ECHO 		############################
 ECHO 		PEROist config.ini Generator
 ECHO 		############################
 ECHO.
-ECHO 1 - Display current offsets
-ECHO 2 - Preview and change offsets for normal sprite
-ECHO 3 - Preview and change offsets for "damaged" sprite
-ECHO 4 - Write offset data as APImodifier.json (NOT WORKING)
-ECHO 5 - Write offset data to %FILENAME%.config.ini
-ECHO 8 - Manually enter known offset values (NOT WORKING)
-ECHO 9 - Reset all input
-ECHO 0 - Quit generator
+ECHO 1 - Preview and change main offsets (standard)
+ECHO 2 - Preview and change main offsets (battle-damage)
+ECHO 3 - Preview and change sub offsets (standard)
+ECHO 4 - Preview and change sub offsets (battle-damage)
+ECHO 5 - Preview and change wedding offsets
+ECHO 6 - Display current offsets [and manually enter offset values]
+ECHO 7 - Write offset data to APImodifier.json [NOT WORKING]
+ECHO 8 - Write offset data to %FILENAME%.config.ini
+ECHO 9 - Reset all input and reload initial values
+ECHO 0 - Quit
 ECHO.
 ECHO Choose from one of the options above and press enter:
 SET "OPTION="
-SET /p OPTION=[1,2,3,4,5,9,0]
-IF /i '%OPTION%'=='1' (
-	GOTO DISPLAY
-) ELSE IF /i '%OPTION%'=='2' (
+SET /p OPTION=[1,2,3,4,5,6,7,8,9,0]
+IF /i '%OPTION%'=='0' (
+	GOTO EXIT
+) ELSE IF /i '%OPTION%'=='1' (
 	SET BACKGROUND="%PARENT%data\room.png"
 	SET SPRITE="%PARENT%temp\!FILENAME!.hack.swf_images\17.png"
-	SET ALIAS="Normal"
+	SET ALIAS="standard"
 	SET /a ORIGIN_X=326
 	SET /a ORIGIN_Y=-19
 	SET /a CURRENT_X=%boko_n_left%
 	SET /a CURRENT_Y=%boko_n_top%
 	SET NEW_X=boko_n_left
 	SET NEW_Y=boko_n_top
+	SET DELTA=1
 	GOTO PROCESS
-) ELSE IF /i '%OPTION%'=='3' (
-	ECHO OPTION %OPTION% selected.
+) ELSE IF /i '%OPTION%'=='2' (
 	SET BACKGROUND="%PARENT%data\room.png"
 	SET SPRITE="%PARENT%temp\!FILENAME!.hack.swf_images\19.png"
-	SET ALIAS="Damaged"
+	SET ALIAS="battledamage"
 	SET /a ORIGIN_X=326
 	SET /a ORIGIN_Y=-45
 	SET /a CURRENT_X=%boko_d_left%
 	SET /a CURRENT_Y=%boko_d_top%
 	SET NEW_X=boko_d_left
 	SET NEW_Y=boko_d_top
+	SET DELTA=2
+	GOTO PROCESS
+) ELSE IF /i '%OPTION%'=='3' (
+	SET BACKGROUND="%PARENT%data\panel.png"
+	SET SPRITE="%PARENT%temp\!FILENAME!.hack.swf_images\13.png"
+	SET ALIAS="standard"
+	SET /a ORIGIN_X=
+	SET /a ORIGIN_Y=
+	SET /a CURRENT_X=%kaisyu_n_left%
+	SET /a CURRENT_Y=%kaisyu_n_top%
+	SET NEW_X=kaisyu_n_left
+	SET NEW_Y=kaisyu_n_top
+	SET DELTA=0
 	GOTO PROCESS
 ) ELSE IF /i '%OPTION%'=='4' (
-	GOTO WRITE_JSON
+	SET BACKGROUND="%PARENT%data\panel.png"
+	SET SPRITE="%PARENT%temp\!FILENAME!.hack.swf_images\15.png"
+	SET ALIAS="battledamage"
+	SET /a ORIGIN_X=
+	SET /a ORIGIN_Y=
+	SET /a CURRENT_X=%kaisyu_d_left%
+	SET /a CURRENT_Y=%kaisyu_d_top%
+	SET NEW_X=kaisyu_d_left
+	SET NEW_Y=kaisyu_d_top
+	SET DELTA=0
+	GOTO PROCESS
 ) ELSE IF /i '%OPTION%'=='5' (
-	GOTO WRITE_INI
+	SET BACKGROUND="%PARENT%data\chapel.png"
+	SET SPRITE="%PARENT%temp\!FILENAME!.hack.swf_images\17.png"
+	SET ALIAS="wedding"
+	SET /a ORIGIN_X=
+	SET /a ORIGIN_Y=
+	SET /a CURRENT_X=%weda_left%
+	SET /a CURRENT_Y=%weda_top%
+	SET NEW_X=weda_left
+	SET NEW_Y=weda_top
+	SET DELTA=3
+	GOTO PROCESS
+) ELSE IF /i '%OPTION%'=='6' (
+	GOTO DISPLAY
+) ELSE IF /i '%OPTION%'=='7' (
+	GOTO WRITE_JSON
 ) ELSE IF /i '%OPTION%'=='8' (
-	GOTO MANUAL_ENTRY
+	GOTO WRITE_INI
 ) ELSE IF /i '%OPTION%'=='9' (
-	ECHO Reinitialising offset values...
-	PAUSE
-	GOTO INIT
-) ELSE IF /i '%OPTION%'=='0 '(
-	GOTO EXIT
+	ECHO Are you sure you want to reset and abandon all changes made so far?
+	SET /p OPTION2=[y/n]
+	IF /i '%OPTION2%'=='y' GOTO INIT
+	GOTO MENU
 ) ELSE (
-	ECHO Please choose one valid option from the list
+	ECHO Please enter one valid option from the list above
 	PAUSE
 	GOTO MENU
 )
 GOTO MENU
 
 :DISPLAY
-ECHO Displaying current values
-ECHO ship_name=%ship_name%
+CLS
+ECHO Displaying active offset values for %FILENAME%.hack.swf
+ECHO boko_n_left=%boko_n_left%  
+ECHO boko_n_top=%boko_n_top%   
 ECHO boko_d_left=%boko_d_left%
 ECHO boko_d_top=%boko_d_top%
-ECHO boko_n_left=%boko_n_left%
-ECHO boko_n_top=%boko_n_top%
-PAUSE
-GOTO MENU
+ECHO map_n_left=%map_n_left%
+ECHO map_n_top=%map_n_top%
+ECHO map_d_left=%map_d_left%
+ECHO map_d_top=%map_d_top%
+ECHO battle_d_top=%battle_d_top%
+ECHO battle_d_left=%battle_d_left%
+ECHO battle_n_top=%battle_n_top%
+ECHO battle_n_left=%battle_n_left%
+ECHO ensyuf_n_left=%ensyuf_n_left%
+ECHO ensyuf_n_top=%ensyuf_n_top%
+ECHO ensyuf_d_left=%ensyuf_d_left%
+ECHO ensyuf_d_top=%ensyuf_d_top%
+ECHO ensyue_n_left=%ensyue_n_left%
+ECHO ensyue_n_top=%ensyue_n_top%
+ECHO kaizo_n_left=%kaizo_n_left%
+ECHO kaizo_n_top=%kaizo_n_top%
+ECHO kaizo_d_left=%kaizo_d_left%
+ECHO kaizo_d_top=%kaizo_d_top%
+ECHO kaisyu_d_left=%kaisyu_d_left%
+ECHO kaisyu_d_top=%kaisyu_d_top%
+ECHO kaisyu_n_left=%kaisyu_n_left%
+ECHO kaisyu_n_top=%kaisyu_n_top%
+ECHO weda_left=%weda_left%
+ECHO weda_top=%weda_top%
+ECHO wedb_left=%wedb_left%
+ECHO wedb_top=%wedb_top%
+
+
+:MANUAL_ENTRY
+ECHO Would you like to manually modify any of these values?
+SET /p OPTION2=[y/n]
+IF /i %OPTION2% neq y GOTO MENU
+SET /p _NAME=Please enter the name of offset you wish to modify: 
+SET /p _VALUE=Please enter the new value of offset you wish to modify: 
+SET %_NAME%=%_VALUE%
+GOTO DISPLAY
 
 :PROCESS
 SET /a ANCHOR_X=%ORIGIN_X%+%CURRENT_X%
@@ -144,24 +207,59 @@ ECHO Are you happy with the results?
 SET /p ACCEPT=[y/n]
 IF /i %ACCEPT%==y GOTO MENU
 CLS
+REM First give new values for currently selected variables
 ECHO Current %NEW_X% is %CURRENT_X%
 ECHO Please enter a new value for %NEW_X%:
 SET /p !NEW_X!=
 ECHO Current %NEW_Y% is %CURRENT_Y%
 ECHO Please enter a new value for %NEW_Y%:
 SET /p !NEW_Y!=
-REM Pipe stored offset values to preview generation
+REM Then calculate the delta values and apply them to derivative values
+IF '%DELTA%'=='0' (
+	ECHO Nothing more to be done, proceed with the loop...
+	) ELSE IF '%DELTA%'=='1' (
+	ECHO Setting derivative offsets for the standard set based on current delta
+	SET /a DELTA_X=!CURRENT_X!-!NEW_X!
+	SET /a DELTA_Y=!CURRENT_T!-!NEW_Y!
+	SET /a map_n_left=!map_n_left!+!DELTA_X!
+	SET /a map_n_top=!map_n_top!+!DELTA_Y!
+	SET /a battle_n_left=!battle_n_left!+!DELTA_X!
+	SET /a battle_n_top=!battle_n_top!+!DELTA_Y!
+	SET /a ensyuf_n_left=!ensyuf_n_left!+!DELTA_X!
+	SET /a ensyuf_n_top=!ensyuf_n_top!+!DELTA_Y!
+	SET /a ensyue_n_left=!ensyue_n_left!+!DELTA_X!
+	SET /a ensyue_n_top=!ensyue_n_top!+!DELTA_Y!
+	SET /a kaizo_n_left=!kaizo_n_left!+!DELTA_X!
+	SET /a kaizo_n_top=!kaizo_n_top!+!DELTA_Y!
+	) ELSE IF '%DELTA%'=='2' (
+	ECHO Setting derivative offsets for the battledamaged set based on current delta
+	SET /a DELTA_X=!CURRENT_X!-!NEW_X!
+	SET /a DELTA_Y=!CURRENT_T!-!NEW_Y!
+	SET /a map_d_left=!map_d_left!+!DELTA_X!
+	SET /a map_d_top=!map_d_top!+!DELTA_Y!
+	SET /a battle_d_left=!battle_d_left!+!DELTA_X!
+	SET /a battle_d_top=!battle_d_top!+!DELTA_Y!
+	SET /a ensyuf_d_left=!ensyuf_d_left!+!DELTA_X!
+	SET /a ensyuf_d_top=!ensyuf_d_top!+!DELTA_Y!
+	SET /a kaizo_d_left=!kaizo_d_left!+!DELTA_X!
+	SET /a kaizo_d_top=!kaizo_d_top!+!DELTA_Y!
+	) ELSE IF '%DELTA%'=='3' (
+	ECHO Applying derivative offsets to wedding cut-scene
+	SET /a DELTA_X=!CURRENT_X!-!NEW_X!
+	SET /a DELTA_Y=!CURRENT_T!-!NEW_Y!
+	SET /a wedb_left=!weda_left!+!delta_X!
+	SET /a wedb_top=!weda_top!+!delta_Y!	
+	) ELSE (
+	ECHO Something went wrong. DELTA flag is either not set or had an unexpected value
+	PAUSE
+	GOTO MENU
+)
+REM Then pipe stored offset values to preview generation
 ECHO Offset values successfully updated. Press any key to generate a new review.
 SET /a CURRENT_X=%NEW_X%
 SET /a CURRENT_Y=%NEW_Y%
 PAUSE
 GOTO PROCESS
-
-REM Manually enter values for each variable
-:MANUAL_ENTRY
-ECHO Choose the value you wish to edit
-PAUSE
-GOTO MENU
 
 REM Write offset data for 74EO.
 :WRITE_JSON
@@ -170,55 +268,51 @@ PAUSE
 GOTO MENU
 
 REM Writing finalised coordinates to %FILENAME%.config.ini
+REM do ensyue_d even exist? 
 :WRITE_INI
+ECHO Applying offsets to 
 CD "%PARENT%output"
 DEL /q %FILENAME%.config.ini
-ECHO Writing new coordinates to %FILENAME%.config.ini
-@echo [info] >%FILENAME%.config.ini
-@echo ship_name=%ship_name%>%FILENAME%.config.ini
+@echo [info]>%FILENAME%.config.ini
+@echo ship_name=%ship_name%>>%FILENAME%.config.ini
 @echo [graph]>>%FILENAME%.config.ini
 @echo boko_d_left=%boko_d_left%>>%FILENAME%.config.ini
 @echo boko_d_top=%boko_d_top%>>%FILENAME%.config.ini
 @echo boko_n_left=%boko_n_left%>>%FILENAME%.config.ini
 @echo boko_n_top=%boko_n_top%>>%FILENAME%.config.ini
-
-REM skip the remaining for now
+@echo ensyue_d_left=%ensyue_d_left%>>%FILENAME%.config.ini
+@echo ensyue_d_top=%ensyue_d_top%>>%FILENAME%.config.ini
+@echo ensyue_n_left=%ensyue_n_left%>>%FILENAME%.config.ini
+@echo ensyue_n_top=%ensyue_n_top%>>%FILENAME%.config.ini
+@echo ensyuf_d_left=%ensyuf_d_left%>>%FILENAME%.config.ini
+@echo ensyuf_d_top=%ensyuf_d_top%>>%FILENAME%.config.ini
+@echo ensyuf_n_left=%ensyuf_n_left%>>%FILENAME%.config.ini
+@echo ensyuf_n_top=%ensyuf_n_top%>>%FILENAME%.config.ini
+@echo kaisyu_d_left=%kaisyu_d_left%>>%FILENAME%.config.ini
+@echo kaisyu_d_top=%kaisyu_d_top%>>%FILENAME%.config.ini
+@echo kaisyu_n_left=%kaisyu_n_left%>>%FILENAME%.config.ini
+@echo kaisyu_n_top=%kaisyu_n_top%>>%FILENAME%.config.ini
+@echo kaizo_d_left=%kaizo_d_left%>>%FILENAME%.config.ini
+@echo kaizo_d_top=%kaizo_d_top%>>%FILENAME%.config.ini
+@echo kaizo_n_left=%kaizo_n_left%>>%FILENAME%.config.ini
+@echo kaizo_n_top=%kaizo_n_top%>>%FILENAME%.config.ini
+@echo map_d_top=%map_d_top%>>%FILENAME%.config.ini
+@echo map_d_left=%map_d_left%>>%FILENAME%.config.ini
+@echo map_n_top=%map_n_top%>>%FILENAME%.config.ini
+@echo map_n_left=%map_n_left%>>%FILENAME%.config.ini
+@echo battle_d_top=%battle_d_top%>>%FILENAME%.config.ini
+@echo battle_d_left=%battle_d_left%>>%FILENAME%.config.ini
+@echo battle_n_top=%battle_n_top%>>%FILENAME%.config.ini
+@echo battle_n_left=%battle_n_left%>>%FILENAME%.config.ini
+@echo weda_left=%weda_left%>>%FILENAME%.config.ini
+@echo weda_top=%weda_top%>>%FILENAME%.config.ini
+@echo wedb_left=%wedb_left%>>%FILENAME%.config.ini
+@echo wedb_top=%wedb_top%>>%FILENAME%.config.ini
+ECHO Saving new coordinates to %PARENT%output\%FILENAME%.config.ini
+PAUSE
 GOTO MENU
 
-@echo battle_d_left= >> %FILENAME%.config.ini
-@echo battle_d.top= >> %FILENAME%.config.ini
-@echo battle_n_left=
-@echo battle_n.top=
 
-ensyue_d_left=
-ensyue_d_top=
-ensyue_n_left=
-ensyue_n_top=
-
-ensyuf_d_left=
-ensyuf_d_top=
-ensyuf_n_left=
-ensyuf_n_top=
-
-kaisyu_d_left=
-kaisyu_d_top=
-kaisyu_n_left=
-kaisyu_n_top=
-
-kaizo_d_left=
-kaizo_d_top=
-kaizo_n_left=
-kaizo_n_top=
-
-map_d_left=
-map_d_top=
-map_n_left=
-map_n_top=
-
-weda_left=
-weda_top=
-wedb_left=
-wedb_top=
 
 :EXIT
 ENDLOCAL
